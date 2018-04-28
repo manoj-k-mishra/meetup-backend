8171ec4de5ab6d5ad614bbe4ad1212780aced0a9
manojmishra571980 <manojmishra571980@gmail.com>
2018-04-28 18:57:28 +1000

    model & controller created

---
+++ server/modules/meetups/controller.js
@@ -0,0 +1,6 @@
+import Meetup from './model';
+
+export const createMeetup = async (req, res) => {
+  const { title, description } = req.body;
+  const newMeetup = new Meetup({ title, description });
+}
