f1c44d91195389f2509184188105403235b4e089
manojmishra571980 <manojmishra571980@gmail.com>
2018-04-28 19:33:09 +1000

    routes created- post and get tested from postman

--- server/index.js
+++ server/index.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import dbConfig from './config/db';
 import middlewaresConfig from './config/middlewares';
+import { MeetupRoutes } from './modules';
 
 const app = express();
 dbConfig();
 middlewaresConfig(app);
+app.use('/api', [MeetupRoutes]);
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT,err=>{
---
+++ server/modules/meetups/index.js
@@ -0,0 +1,7 @@
+
+import MeetupRoutes from './routes';
+
+export {
+  MeetupRoutes,
+
+};
