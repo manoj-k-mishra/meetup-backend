d76a0608f723b0670e688cbe0339ce7ae37de0ad
manojmishra571980 <manojmishra571980@gmail.com>
2018-04-28 13:09:36 +1000

    mongodb running

--- server/index.js
+++ server/index.js
@@ -1,8 +1,11 @@
 import express from 'express';
+import dbConfig from './config/db';
 
 const app = express();
+dbConfig();
+
 const PORT = process.env.PORT || 3000
 app.listen(PORT,err=>{
-if(err){console.error(err);} else {console.log(`App listenining on port: ${PORT}`);}
+if(err){console.error(err);} else {console.log(`/server/index.js--App listenining on port: ${PORT}`);}
 
 });
