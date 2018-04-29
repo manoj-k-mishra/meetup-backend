ac2997b58cf9457c01e2d89f401210c7c2943e59
manojmishra571980 <manojmishra571980@gmail.com>
2018-04-29 12:10:58 +1000

    create group working(group model, controller, routes added)

---
+++ server/modules/groups/controller.js
@@ -0,0 +1,27 @@
+import Group from './model';
+
+
+export const createGroup = async (req, res) =>
+{  const {  name,  description } = req.body;
+   if (!name)
+   { return res.status(400).json({ error: true, message: 'Name must be provided!' });
+   } else if (typeof name !== 'string')
+   { return res.status(400).json({ error: true, message: 'Name must be a string!' });
+   } else if (name.length < 5)
+   { return res.status(400).json({ error: true, message: 'Name must have 5 characters long!' });
+   }
+
+  if (!description)
+  { return res.status(400).json({ error: true, message: 'Description must be provided!' });
+  } else if (typeof description !== 'string')
+  { return res.status(400).json({ error: true, message: 'Description must be a string!' });
+  } else if (description.length < 10)
+  {  return res.status(400).json({  error: true,  message: 'Description must have 10 characters long!', });
+  }
+
+  const group = new Group({ name, description });
+
+  try { return res.status(201).json({ error: false, group: await group.save() });
+  } catch (e) { return res.status(400).json({ error: true, message: 'Error when created group' }); }
+}; //create group finish
+
--- server/modules/meetups/model.js
+++ server/modules/meetups/model.js
@@ -1,7 +1,9 @@
 import mongoose, { Schema } from 'mongoose';
 
-const MeetupSchema = new Schema({
-  title: {  type: String, required: true, },
-  description: {  type: String,  required: true,},
-});
+const MeetupSchema = new Schema(
+{ title: {  type: String, required: true, },
+  description: { type: String,  required: true,},
+  eventDate: {  type: Date, },
+}, { timestamps: true });
+
 export default mongoose.model('Meetup', MeetupSchema);
